6e24c729552edfc825f2477408568af8ad961371
Jonathan Ramirez <84485168+jonathan6ramirez@users.noreply.github.com>
2022-01-26 17:57:57 -0600

    Added comments

--- front-end/src/layout/reservations/CreateReservation.js
+++ front-end/src/layout/reservations/CreateReservation.js
@@ -1,5 +1,11 @@
 import React from "react"
 
+// This is going to be the main page for the `reservations/new` route
+// The create and edit form will be the same component that will display
+// the correct paramters based on if there is a reservation parameter/state
+// present.
+
+
 function CreateReservation() {
     return (
         <h2>This is the reservation page!</h2>
