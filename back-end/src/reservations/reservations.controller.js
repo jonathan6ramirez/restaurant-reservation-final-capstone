c741853f985699790313646015a2139eb7d33635
Jonathan Ramirez <84485168+jonathan6ramirez@users.noreply.github.com>
2022-01-27 15:00:07 -0600

    Added the template for the valid properties. Also added the validator for the input.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,8 +1,38 @@
 const reservationsService = require("./reservations.service");
 
-/**
- * List handler for reservation resources
- */
+//Error handlers
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+
+
+// Templates to validate data
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+];
+
+//Middleware
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter(
+    (field) => !VALID_PROPERTIES.includes(field)
+  );
+
+  if (invalidFields.length)
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  next();
+}
+
+// CRUD Functions
+
 async function list(req, res) {
   const data = await reservationsService.list();
   res.json({ data });
