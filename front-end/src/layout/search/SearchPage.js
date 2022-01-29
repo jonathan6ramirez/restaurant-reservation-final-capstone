3f7030151046571fac071167ceb38434f1d2d6e6
Jonathan Ramirez <84485168+jonathan6ramirez@users.noreply.github.com>
2022-01-29 16:51:44 -0600

    Added some pseudo code to the SearchPage component.

--- front-end/src/layout/search/SearchPage.js
+++ front-end/src/layout/search/SearchPage.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 function SearchPage() {
-    
+
+    // This is the initial search state
+    const initialFormState = {
+        mobile_number: "",
+    };
+    // This is the state of the form
+    const [form, setForm] = useState({ ...initialFormState });
+    // This handles when the form changes and any input is detected
+    const handleChange = ({ target }) => {
+        setForm({
+            ...form,
+            [target.name]: target.value,
+        });
+    };
+
+    // This is the state for the reservations and to show them
+    const [empty, setEmpty] = useState(null);
+    const [reservations, setReservations] = useState([]);
+    const [loaded, setLoaded] = useState(null);
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        // Set the searched state to true so the reservations will show
+        clickedSearch(true);
+        // check to see if the phone number is in the correct format
+        // make the call to the api for the reservations
+        // check to see if the call is empty
+            // if it is empty set the empty state to `message: No reservations found`
+        // set the state of the reservations to the call if it isnt empty
     }
     return (
         <React.Fragment>
@@ -29,6 +55,7 @@ function SearchPage() {
 
             </Form>
 
+            {/*Depending on the state of the reservations output them*/}
         </React.Fragment>
     )
 };
