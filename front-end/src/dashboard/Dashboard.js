74624db5836581fc65ea968edda70c23d6e0181b
JonathanRamirez <ramirezjonathan21@gmail.com>
2022-02-07 11:08:07 -0600

    Added placeholder values for the tables inside the Dashboard file, as well as adding the render tables function.

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -15,7 +15,32 @@ function Dashboard({ date }) {
   const [reservationsError, setReservationsError] = useState(null);
 
   useEffect(loadDashboard, [date]);
-  
+
+  let tables = [
+    {
+      table_name: "Bar #1",
+      capacity: 1,
+      is_occupied: null,
+    },
+    {
+      table_name: "Bar #2",
+      capacity: 1,
+      is_occupied: null,
+    },
+    {
+      table_name: "#1",
+      capacity: 6,
+      is_occupied: null,
+    },
+    {
+      table_name: "#2",
+      capacity: 6,
+      is_occupied: null,
+    }
+  ]
+
+
+  // * Mapper functions
   const mapOutReservations = (reservation, index) => {
     return (
       <div key={index} className="row justify-content-center" >
@@ -27,6 +52,21 @@ function Dashboard({ date }) {
               People: {reservation.people}
             </Card.Text>
             <Button variant="primary" href={`reservations/${reservation.reservation_id}/seat`} >Seat</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  }
+
+  const mapOutTables = (table, index) => {
+    const occupied = table.is_occupied ? "Occupied": "Free"
+    return (
+      <div key={index} className="row" >
+        <Card>
+          <Card.Header as="h5">Table: {table.table_name}</Card.Header>
+          <Card.Body>
+            <Card.Title>Capacity: {table.capacity}</Card.Title>
+            <Card.Text data-table-id-status={table.table_id} >Is Occupied: {occupied}</Card.Text>
           </Card.Body>
         </Card>
       </div>
@@ -50,6 +90,11 @@ function Dashboard({ date }) {
       </div>
       <ErrorAlert error={reservationsError} />
       {reservations.map(mapOutReservations)}
+
+      <div className="d-md-flex mt-4 mb-3">
+        <h4 className="mb-0">Tables Available: </h4>
+      </div>
+      {tables.map(mapOutTables)}
     </main>
   );
 }
